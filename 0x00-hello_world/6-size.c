166b6581736b3f9f0e820c74fee096f7fb0cefea
Edwin Agana <edwinagana@gmail.com>
2022-06-30 14:50:07 -0700

    C program that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of varius  types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+printf("size of a char: xd byte(s)\n", sizeof(char));
+printf("size of an int: xd byte(s)\n", sizeof(int));
+printf("size of a long int: xd byte(s)\n", sizeof(long int));
+printf("size of a long long int: xd byte(s)\n", sizeof(long long int));
+printf("size of a float: xd byte(s)\n", sizeof(foat));
+}
