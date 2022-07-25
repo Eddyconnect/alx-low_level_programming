f8dcb39ce91b8808245bbcf61c9cd00c2f1cb78d
Edwin Agana <edwinagana@gmail.com>
2022-07-25 05:57:44 -0700

    Task corrected

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,19 +3,18 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes
- * it with a specific char
- * @size: size of array
- * @c: char integer
- * Return: pointer to array or NULL if fail
+ *create_array - creates an array of char
+ *@size: size of array
+ *@c: char integer
+ *Return: pointer to array or NULL if fail
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *pt = malloc(size);
-	if (size == 0 || pt == 0) /*Confirm malloc was successful*/
-		return (0);
-	while (size--)
-		pt[size] = c;
-	return (pt);
+char *p = malloc(size);
+if (size == 0 || p == 0)
+return (0);
+while (size--)
+p[size] = c;
+return (p);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,37 +1,24 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
-* _strdup - returns a pointer to a newly allocated space in memory, 
-* which contains a copy of the string given as a parameter.
-* @str: string we need to duplicate
+* _strdup - Entry point
+*@str: string we need to duplicate
 *
-* Return: pointer to a newly allocated space in memory.
+* Return: Return success.
 **/
-
 char *_strdup(char *str)
 {
-	char *a;
-	int i, c;
-
-	if (str == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0'; i++) /*Determine number of characters of str*/
-	{
-		continue;
-	}
-
-	a = malloc(i * sizeof(*a) + 1);
-	if (a == NULL) /*Confirm malloc did not fail*/
-	{
-		return (NULL);
-	}
-
-	for (c = 0; c < i; c++)
-	{
-		a[c] = str[c];
-	}
-
-	a[c] = '\0';
-	return (a);
+char *a;
+int i, c;
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++)
+;
+a = malloc(i * sizeof(*a) + 1);
+if (a == NULL)
+return (NULL);
+for (c = 0; c < i; c++)
+a[c] = str[c];
+a[c] = '\0';
+return (a);
 }
