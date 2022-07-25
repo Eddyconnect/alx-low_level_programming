83de74e9f57735b6bf59a8840ff9b6b5e0c049f6
Edwin Agana <edwinagana@gmail.com>
2022-07-25 06:07:30 -0700

    Task completed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,55 +1,52 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-* _strlen - finds length of a given string.
-* @s: pointer to string of char type
+* _strlen - Swaps integers wih pointers.
 *
-* Return: length of given string.
+* @s: is a pointer to a char
+*
+* Return: Always 0.
 */
-
 int _strlen(char *s)
 {
-	int i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
+int i = 0;
+while (*(s + i) != '\0')
+i++;
+return (i);
 }
-
-
 /**
-* str_concat - Concatenates 2 strings.
+* str_concat - Concatenates 2 strings..
+*
 * @s1: First string.
 * @s2: Second string.
 *
 * Return: Returns the created array.
 **/
-
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1, len2, tlen;
-	char *ar;
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	tlen = (len1 + len2)+1;
-	ar = malloc(tlen * sizeof(char));
-	if (ar == NULL)
-		return (NULL);
-	else
-	{
-		for (i = 0; i < len1; i++)
-			ar[i] = s1[i];
-		for (j = 0; j < len2; j++)
-		{
-			ar[i] = s2[j];
-			i++;
-		}
-		ar[i + 1] = '\0';
-		return (ar);
-	}
+int i, j, size1, size2, totSize;
+char *ar;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+size1 = _strlen(s1);
+size2 = _strlen(s2);
+totSize = (size1 + size2)+1;
+ar = malloc(totSize *sizeof(char));
+if (ar == NULL)
+return (NULL);
+else
+{
+for (i = 0; i < size1; i++)
+ar[i] = s1[i];
+for (j = 0; j < size2; j++)
+{
+ar[i] = s2[j];
+i++;
+}
+ar[i + 1] = '\0';
+return (ar);
+}
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,21 +2,18 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a two dimensional array
+ * free_grid - frees a two dimensional
  * @grid: the address of the two dimensional array
  * @height: height of the grid
- *
  * Return: void
  */
 
 void free_grid(int **grid, int height)
 {
-	int x;
-
-	for (x = 0; x < height; x++)
-	{
-		free(grid[x]);
-	}
-
-	free(grid);
+int x;
+for (x = 0; x < height; x++)
+{
+free(grid[x]);
+}
+free(grid);
 }
