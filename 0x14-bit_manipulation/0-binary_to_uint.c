b71b1e3aee6e68d990e3a8a66fb7593771b202c1
Eddyconnect <edwinagana@gmail.com>
2022-08-18 18:19:43 +0100

    task updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,47 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
+ * binary_to_uint - Convert a binary number to an unsigned int
+ * @b: A string of 0 and 1 chars
  *
- * Return: unsigned int.
+ * Return: The converted number if successful, 0 otherwise
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int uint = 0, base = 1;
+	int length, i;
 
 	if (!b)
 		return (0);
 
-	ui = 0;
+	length = _strlen(b);
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+	for (i = 0; i < length; i++)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
 
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+	for (i = length - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+			uint += base;
+		base *= 2;
 	}
 
-	return (ui);
+	return (uint);
+}
+
+/**
+ * _strlen - Return the length of a string
+ * @s: The string to check
+ *
+ * Return: The length of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
 }
